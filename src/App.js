788bb9d5a8b615635113dcdbf509a1e9a3e02318
Kris de Rycke <derycke.kris@gmail.com>
2019-08-23 14:24:52 +0200

    add submit to form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        name: "Dee",
-        job: "Dancer"
-      },
-      {
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
+    characters: []
   };
 
   removeCharacter = index => {
@@ -32,6 +16,10 @@ class App extends Component {
     });
   };
 
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
   render() {
     const { characters } = this.state;
 
@@ -41,6 +29,7 @@ class App extends Component {
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
