e48185c10e3067710286c72b843819c05b9571e5
Kris de Rycke <derycke.kris@gmail.com>
2019-08-23 09:45:24 +0200

    Updates

---
+++ precache-manifest.4f60d73b0a1859a9cddf695b5806760e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36da952914a3c74e5ed4004221b925cb",
+    "url": "/krisderycke/Todo-app-React/index.html"
+  },
+  {
+    "revision": "abb6709c9c73eca0f953",
+    "url": "/krisderycke/Todo-app-React/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2286cd7ede943e022602",
+    "url": "/krisderycke/Todo-app-React/static/js/2.ff9eaa9c.chunk.js"
+  },
+  {
+    "revision": "abb6709c9c73eca0f953",
+    "url": "/krisderycke/Todo-app-React/static/js/main.d417f1b4.chunk.js"
+  },
+  {
+    "revision": "bfbc5b90486d76f4453b",
+    "url": "/krisderycke/Todo-app-React/static/js/runtime~main.4c37bf68.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/krisderycke/Todo-app-React/precache-manifest.4f60d73b0a1859a9cddf695b5806760e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/krisderycke/Todo-app-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
